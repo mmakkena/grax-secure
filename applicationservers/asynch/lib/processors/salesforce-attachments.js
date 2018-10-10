bb8f39c9ce49dd0a558997423ab7a1d25da3aa70
gaskaj <joegaska@gmail.com>
2018-10-10 16:45:39 -0400

    Fixed S3 default settings bug

---
+++ applicationservers/asynch/lib/processors/salesforce-attachments.js
@@ -0,0 +1,16 @@
+
+/* --------------------------------------------------------------------------
+ * This file is subject to the terms and conditions defined in
+ * file https://github.com/HardingPoint/GRAXForSFDC/blob/master/LICENSE,
+ * which is part of this source code package.
+ * --------------------------------------------------------------------------
+ *          Copyright (c) 2017-2020 Harding Point
+ *                  All rights reserved
+ *             Built: Wed Oct 10 2018 16:44:09 GMT-0400 (EDT)
+ * --------------------------------------------------------------------------
+ * Support:	Support@Grax.io
+ * Website:	https://grax.io
+ * Github:	https://github.com/HardingPoint/grax-secure
+ * License:	https://github.com/HardingPoint/grax-secure/blob/master/LICENSE
+ * --------------------------------------------------------------------------*/
+
